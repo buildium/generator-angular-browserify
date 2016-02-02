accdd5fad94d02d32663041ff7d4a1f41739b4a1
Michael DeWitt <mdewitt07@gmail.com>
2016-02-02 12:48:13 -0500

    fix formatting

--- component/index.js
+++ component/index.js
@@ -1,22 +1,23 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-	constructor: function () {
-    	generators.Base.apply(this, arguments);
-		  this.argument('componentName', { type: String, required: true });
-  	},
-	createHtmlandLessFiles: function() {
-      var name = this.componentName;
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    this.argument('componentName', { type: String, required: true });
+  },
+  createHtmlandLessFiles: function() {
+    var name = this.componentName;
 
-  		this.fs.copyTpl(
-  			this.templatePath('component.html'),
-  			this.destinationPath(name + '/' + name + '.html')
-  		);
-  		this.fs.copyTpl(
-  			this.templatePath('component.less'),
-  			this.destinationPath(name + '/' + name + '.less')
-  		);
-	},
+    this.fs.copyTpl(
+      this.templatePath('component.html'),
+      this.destinationPath(name + '/' + name + '.html')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('component.less'),
+      this.destinationPath(name + '/' + name + '.less')
+    );
+  },
   createJsFile: function () {
     var done = this.async();
     var name = this.componentName;
@@ -27,15 +28,15 @@ module.exports = generators.Base.extend({
       message : 'What would you like to call the main function in this component?',
       default : 'NewComponent'
     }, function (answers) {
-      this.fs.copyTpl(
-        this.templatePath('component.js'),
-        this.destinationPath(name + '/' + name + '.js'),
+    this.fs.copyTpl(
+      this.templatePath('component.js'),
+      this.destinationPath(name + '/' + name + '.js'),
         {templateFileName: name, methodName: answers.methodName}
       );
       done();
     }.bind(this));
   },
-	complete: function() {
-		this.log('Successfully created new component!');
-	}
+  complete: function() {
+    this.log('Successfully created new component!');
+  }
 });
