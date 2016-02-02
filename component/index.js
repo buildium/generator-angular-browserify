9803a96f21f5dde24684291fb429f9b55d0a21cd
Michael DeWitt <mdewitt07@gmail.com>
2016-02-02 14:31:54 -0500

    refactor, and add option for LESS files

--- component/index.js
+++ component/index.js
@@ -1,39 +1,46 @@
 var generators = require('yeoman-generator');
 var chalk = require('chalk');
 
+var prompts = [{
+      type    : 'confirm',
+      name    : 'addLessFile',
+      message : 'Would you like to add a LESS file to this component?',
+      default : true
+    }, {
+      type    : 'input',
+      name    : 'methodName',
+      message : 'What would you like to call the main function in this component?',
+      default : 'NewComponent'
+    }];
+
 module.exports = generators.Base.extend({
   constructor: function () {
     generators.Base.apply(this, arguments);
     this.argument('componentName', { type: String, required: true });
   },
-  createHtmlandLessFiles: function() {
-    var name = this.componentName;
-
-    this.fs.copyTpl(
-      this.templatePath('component.html'),
-      this.destinationPath(name + '/' + name + '.html')
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('component.less'),
-      this.destinationPath(name + '/' + name + '.less')
-    );
-  },
-  createJsFile: function () {
+  createFiles: function() {
     var done = this.async();
     var name = this.componentName;
 
-    this.prompt({
-      type    : 'input',
-      name    : 'methodName',
-      message : 'What would you like to call the main function in this component?',
-      default : 'NewComponent'
-    }, function (answers) {
-    this.fs.copyTpl(
-      this.templatePath('component.js'),
-      this.destinationPath(name + '/' + name + '.js'),
-        {templateFileName: name, methodName: answers.methodName}
+    this.prompt(prompts, function promptCallback(answers) {
+      this.fs.copyTpl(
+        this.templatePath('component.html'),
+        this.destinationPath(name + '/' + name + '.html')
       );
+
+      if(answers.addLessFile) {
+        this.fs.copyTpl(
+          this.templatePath('component.less'),
+          this.destinationPath(name + '/' + name + '.less')
+        );
+      }
+
+      this.fs.copyTpl(
+        this.templatePath('component.js'),
+        this.destinationPath(name + '/' + name + '.js'),
+          {templateFileName: name, methodName: answers.methodName}
+      );
+
       done();
     }.bind(this));
   },
