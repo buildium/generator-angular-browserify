da5a165c38a90f0a5141fd53f08b710993a5db36
mndewitt <mdewitt07@gmail.com>
2016-03-23 15:34:41 -0400

    add args for page/service generators, and write unit tests

--- page/index.js
+++ page/index.js
@@ -1,6 +1,7 @@
 var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var helpers = require('../lib/helpers');
+var skipAllPrompts = false;
 
 var prompts = [{
       type    : 'input',
@@ -18,42 +19,69 @@ var prompts = [{
       message : 'What would you like to call the main function on this page?',
       default : 'Newpage'
     }];
+    
+ //Helper methods   
+function createFilesHelper(filePath, addLess, methodName, name) {
+  this.fs.copyTpl(
+    this.templatePath('page.html'),
+    this.destinationPath(filePath + name + '/' + name + '.html')
+  );
 
+  if(addLess) {
+    this.fs.copyTpl(
+      this.templatePath('page.less'),
+      this.destinationPath(filePath + name + '/' + name + '.less')
+    );
+  }
+
+  this.fs.copyTpl(
+    this.templatePath('page.js'),
+    this.destinationPath(filePath + name  + '/' + name + '.js'),
+    {templateFileName: name, methodName: methodName}
+  );
+}
+
+//Main generator
 module.exports = generators.Base.extend({
   constructor: function () {
     generators.Base.apply(this, arguments);
     this.argument('pageName', { type: String, required: true });
+    
+    //Optional args for using VS shortcuts
+    this.argument('filePathArg', { type: String, required: false });
+    this.argument('addLessArg', { type: String, required: false });
+    this.argument('methodNameArg', { type: String, required: false });
+
   },
   createFiles: function() {
     var done = this.async();
     var name = this.pageName;
-
-    this.prompt(prompts, function promptCallback(answers) {
-      var filePath = helpers.addTrailingSlashToFilePath(answers.filePath);
-      
-      this.fs.copyTpl(
-        this.templatePath('page.html'),
-        this.destinationPath(filePath + name + '/' + name + '.html')
-      );
-
-      if(answers.addLessFile) {
-        this.fs.copyTpl(
-          this.templatePath('page.less'),
-          this.destinationPath(filePath + name + '/' + name + '.less')
-        );
-      }
-
-      this.fs.copyTpl(
-        this.templatePath('page.js'),
-        this.destinationPath(filePath + name  + '/' + name + '.js'),
-          {templateFileName: name, methodName: answers.methodName}
-      );
-
-      done();
-    }.bind(this));
+    var filePath = this.filePathArg;
+    var addLess = this.addLessArg;
+    var methodName = this.methodNameArg;
+    
+    //If we are missing any of the args, run the prompts
+    if(!filePath || !addLess || !methodName) {
+      this.prompt(prompts, function promptCallback(answers) {
+        filePath =  helpers.addTrailingSlashToFilePath(answers.filePath);
+        addLess = answers.addLessFile;
+        methodName = answers.methodName;
+        createFilesHelper.apply(this, [filePath, addLess, methodName, name]);
+        done();
+      }.bind(this)); 
+    } else {
+        skipAllPrompts = true;
+        //Convert the string arg for less to a bool
+        addLess = (addLess === 'true');
+        filePath = helpers.addTrailingSlashToFilePath(filePath);
+        createFilesHelper.apply(this, [filePath, addLess, methodName, name]);
+        done();
+    }
   },
   checkVSConfig: function() {
-    helpers.checkVSConfig(this);
+    if(!skipAllPrompts) {
+      helpers.checkVSConfig(this);
+    }
   },
   complete: function() {
     this.log(chalk.bold.green('Successfully created new page...'));
--- tests/page.js
+++ tests/page.js
@@ -2,15 +2,15 @@ var helpers = require('yeoman-test');
 var path = require('path');
 var assert = require('yeoman-assert');
 
-var expectedFilesWithoutLess = ['test/my-component/my-component.js', 'test/my-component/my-component.html'];
-var expectedFilesWithLess = ['test/my-component/my-component.js', 'test/my-component/my-component.html', 'test/my-component/my-component.less'];
+var expectedFilesWithoutLess = ['test/my-page/my-page.js', 'test/my-page/my-page.html'];
+var expectedFilesWithLess = ['test/my-page/my-page.js', 'test/my-page/my-page.html', 'test/my-page/my-page.less'];
 
-describe('angular-browserify:component', function () {
+describe('angular-browserify:page', function () {
   //Using the prompts...
   describe('when using just the name argument, and choosing to add a less file', function () {
     before(function beforeTest(done) {
-      helpers.run(path.join(__dirname, '../component'))
-        .withArguments(['my-component'])
+      helpers.run(path.join(__dirname, '../page'))
+        .withArguments(['my-page'])
         .withPrompts({ 
             filePath: 'test',
             addLessFile: true,
@@ -19,19 +19,19 @@ describe('angular-browserify:component', function () {
         .on('end', done);
     });
 
-    it('generates the component files', function () {
+    it('generates the page files', function () {
       assert.file(expectedFilesWithLess);
     });
     
     it('adds the method name to the JS file', function () {
-      assert.fileContent('test/my-component/my-component.js', /myMethod/);
+      assert.fileContent('test/my-page/my-page.js', /myMethod/);
     });
   });
   
   describe('when using just the name argument, and choosing not to add a less file', function () {
     before(function beforeTest(done) {
-      helpers.run(path.join(__dirname, '../component'))
-        .withArguments(['my-component'])
+      helpers.run(path.join(__dirname, '../page'))
+        .withArguments(['my-page'])
         .withPrompts({ 
             filePath: 'test',
             addLessFile: false,
@@ -40,39 +40,39 @@ describe('angular-browserify:component', function () {
         .on('end', done);
     });
 
-    it('generates the component files without a less file', function () {
+    it('generates the page files without a less file', function () {
       assert.file(expectedFilesWithoutLess);
-      assert.noFile('test/my-component/my-component.less');
+      assert.noFile('test/my-page/my-page.less');
     });
   });
   
   //Using arguments instead of prompts
   describe('when using arguments instead of prompts, and choosing to add a less file', function () {
     before(function beforeTest(done) {
-      helpers.run(path.join(__dirname, '../component'))
-        .withArguments(['my-component', 'test', 'true', 'myMethod'])
+      helpers.run(path.join(__dirname, '../page'))
+        .withArguments(['my-page', 'test', 'true', 'myMethod'])
         .on('end', done);
     });
 
-    it('generates the component files', function () {
+    it('generates the page files', function () {
       assert.file(expectedFilesWithLess);
     });
     
     it('adds the method name to the JS file', function () {
-      assert.fileContent('test/my-component/my-component.js', /myMethod/);
+      assert.fileContent('test/my-page/my-page.js', /myMethod/);
     });
   });
   
   describe('when using arguments instead of prompts, and choosing not to add a less file', function () {
     before(function beforeTest(done) {
-      helpers.run(path.join(__dirname, '../component'))
-        .withArguments(['my-component', 'test', 'false', 'myMethod'])
+      helpers.run(path.join(__dirname, '../page'))
+        .withArguments(['my-page', 'test', 'false', 'myMethod'])
         .on('end', done);
     });
 
-    it('generates the component files without a less file', function () {
+    it('generates the page files without a less file', function () {
       assert.file(expectedFilesWithoutLess);
-      assert.noFile('test/my-component/my-component.less');
+      assert.noFile('test/my-page/my-page.less');
     });
   });
 });
