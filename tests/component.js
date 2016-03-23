1b85200aa133b5c8fa070b55dc72cdcf6f12e0f3
mndewitt <mdewitt07@gmail.com>
2016-03-23 09:59:38 -0400

    add unit tests

---
+++ tests/component.js
@@ -0,0 +1,79 @@
+var helpers = require('yeoman-test');
+var path = require('path');
+var assert = require('yeoman-assert');
+
+describe('angular-browserify:component', function () {
+  //Using the prompts...
+  describe('when using just the name argument, and choosing to add a less file', function () {
+    before(function beforeTest(done) {
+      helpers.run(path.join(__dirname, '../component'))
+        .withArguments(['my-component'])
+        .withPrompts({ 
+            filePath: 'test',
+            addLessFile: true,
+            methodName: 'myMethod'  
+        })
+        .on('end', done);
+    });
+
+    it('generates the component files', function () {
+      assert.file(['test/my-component/my-component.js', 
+                  'test/my-component/my-component.html', 
+                  'test/my-component/my-component.less']);
+    });
+    
+    it('adds the method name to the JS file', function () {
+      assert.fileContent('test/my-component/my-component.js', /myMethod/);
+    });
+  });
+  
+  describe('when using just the name argument, and choosing not to add a less file', function () {
+    before(function beforeTest(done) {
+      helpers.run(path.join(__dirname, '../component'))
+        .withArguments(['my-component'])
+        .withPrompts({ 
+            filePath: 'test',
+            addLessFile: false,
+            methodName: 'myMethod'  
+        })
+        .on('end', done);
+    });
+
+    it('generates the component files without a less file', function () {
+      assert.file(['test/my-component/my-component.js', 'test/my-component/my-component.html']);
+      assert.noFile('test/my-component/my-component.less');
+    });
+  });
+  
+  //Using arguments instead of prompts
+  describe('when using arguments instead of prompts, and choosing to add a less file', function () {
+    before(function beforeTest(done) {
+      helpers.run(path.join(__dirname, '../component'))
+        .withArguments(['my-component', 'test', 'true', 'myMethod'])
+        .on('end', done);
+    });
+
+    it('generates the component files', function () {
+      assert.file(['test/my-component/my-component.js', 
+                  'test/my-component/my-component.html', 
+                  'test/my-component/my-component.less']);
+    });
+    
+    it('adds the method name to the JS file', function () {
+      assert.fileContent('test/my-component/my-component.js', /myMethod/);
+    });
+  });
+  
+  describe('when using arguments instead of prompts, and choosing not to add a less file', function () {
+    before(function beforeTest(done) {
+      helpers.run(path.join(__dirname, '../component'))
+        .withArguments(['my-component', 'test', 'false', 'myMethod'])
+        .on('end', done);
+    });
+
+    it('generates the component files without a less file', function () {
+      assert.file(['test/my-component/my-component.js', 'test/my-component/my-component.html']);
+      assert.noFile('test/my-component/my-component.less');
+    });
+  });
+});
